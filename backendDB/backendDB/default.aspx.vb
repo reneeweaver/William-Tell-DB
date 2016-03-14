1c905446d98b2936e8642c44f4aa944519130c4e
thegeekkid <brian@geekkidconsulting.com>
2016-03-14 18:36:29 -0500

    Declared vars for each piece of information.

--- backendDB/backendDB/default.aspx.vb
+++ backendDB/backendDB/default.aspx.vb
@@ -1,7 +1,19 @@
 ﻿Public Class _default
     Inherits System.Web.UI.Page
+    Dim punchdate As Date
+    Dim in1 As String
+    Dim in2 As String
+    Dim in3 As String
+    Dim in4 As String
+    Dim out1 As String
+    Dim out2 As String
+    Dim out3 As String
+    Dim out4 As String
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+    Protected Sub process_save()
 
     End Sub
 
