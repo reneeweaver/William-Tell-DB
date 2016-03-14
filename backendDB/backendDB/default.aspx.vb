c5fcce95dcbd8bff93224e584b2d4b21a90a3d93
thegeekkid <brian@geekkidconsulting.com>
2016-03-14 18:40:42 -0500

    pulled data in from post method

--- backendDB/backendDB/default.aspx.vb
+++ backendDB/backendDB/default.aspx.vb
@@ -11,7 +11,15 @@
     Dim out4 As String
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        punchdate = Request.QueryString("date")
+        in1 = Request.QueryString("in1")
+        in2 = Request.QueryString("in2")
+        in3 = Request.QueryString("in3")
+        in4 = Request.QueryString("in4")
+        out1 = Request.QueryString("out1")
+        out2 = Request.QueryString("out2")
+        out3 = Request.QueryString("out3")
+        out4 = Request.QueryString("out4")
     End Sub
     Protected Sub process_save()
 
